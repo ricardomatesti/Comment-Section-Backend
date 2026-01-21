bdcfe5d6fd5c3958d81b6e13488005b5ddd26a81
ricardomatesti <ricardosierra1145@gmail.com>
2026-01-21 22:08:59 +0100

    Added Procfile

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
     }
 }
 
+if os.getenv("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

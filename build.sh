8263533239c56746e9795f3bdf4123d659b99286
ricardomatesti <ricardosierra1145@gmail.com>
2026-01-21 22:39:02 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running migrations..."
+python manage.py migrate --no-input
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
